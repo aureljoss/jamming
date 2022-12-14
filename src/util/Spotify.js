b60b6716f5d940aa446587215c49512878ae0d7f
aureljoss <josserand.n.aurelie@gmail.com>
2022-12-13 22:56:33 -0500

    Starts implementing search request

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,3 +1,5 @@
+import SearchBar from "../Components/SearchBar/SearchBar";
+
 let accessToken;
 const clientId='05b99b47d0384570855ca50b0538ead7';
 const redirectURI="http://localhost:3000/";
@@ -5,7 +7,7 @@ const redirectURI="http://localhost:3000/";
 const Spotify={
 
     getAccessToken(){
-    if (accessToken){return accessToken}}
+    if (accessToken){return accessToken;}
 
     // check for access token match
     const accessTokenMatch=window.location.href.match(/access_token=([^&]*)/);
@@ -18,9 +20,17 @@ const Spotify={
         window.setTimeout(() => accessToken = '', expiresIn * 1000);
         window.history.pushState('Access Token', null, '/');
         return accessToken;
-    } else{
+    } else {
         const accessURL="https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}";
         window.location=accessURL;
+    }
+
+    search(term){
+        const accessToken=Spotify.getAccessToken();
+        return fetch('https://api.spotify.com/v1/search?type=track&q=${term}',
+        {headers: {
+            Authorization: 'Bearer ${accessToken}'
+        }})
     }
 
 }
