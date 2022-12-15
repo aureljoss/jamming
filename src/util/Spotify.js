99292afeac0a3d9fe87597365fa693df957ab475
aureljoss <josserand.n.aurelie@gmail.com>
2022-12-14 22:25:17 -0500

    Fixes typos. Convert response to JSON.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -13,7 +13,7 @@ const Spotify={
     const accessTokenMatch=window.location.href.match(/access_token=([^&]*)/);
     const expiresInMatch=window.location.href.match(/expires_in=([^&]*)/);
 
-    if(accessTokenMatch && expriesInMatch){
+    if(accessTokenMatch && expiresInMatch){
         accessToken=accessTokenMatch[1];
         const expiresIn=Number(expiresInMatch[1]);
         //clears the parameters, allowing us to grab a new access token when it expires.
@@ -24,15 +24,27 @@ const Spotify={
         const accessURL="https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}";
         window.location=accessURL;
     }
+},
 
     search(term){
         const accessToken=Spotify.getAccessToken();
         return fetch('https://api.spotify.com/v1/search?type=track&q=${term}',
         {headers: {
             Authorization: 'Bearer ${accessToken}'
-        }})
-    }
-
+        }}).then(response=>{
+            return response.json();
+        }).then(jsonResponse=>{
+            if(!jsonResponse.tracks){return [];}
+            return jsonResponse.tracks.items.map(track=>({
+                id:track.id,
+                name:track.name,
+                artist:track.artists[0].name,
+                album:track.album.name,
+                uri:track.uri
+            }));
+        });
+    },
+    
 }
 
 export default Spotify;
